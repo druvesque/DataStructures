e192b8e6e0a82ba196638d4c49bfa975b70bba9a
druvesque <dhruvsachdeva6480@gmail.com>
2025-12-29 11:06:51 +0530

    note

---
+++ short_notes.c
@@ -0,0 +1,15 @@
+// SORTING
+// 1. BUBBLE SORT
+//    - Intuition: maximum element pushed to it's position in each pass
+//    - Time Complexity
+//      a) Worst Case: O(n²)
+//      b) Best Case: Ω(n)
+//      c) Average Case: Θ(n²)
+// 2. SELECTION SORT
+//    - Intuition: minimum element sorted into it's position in each pass
+//    - Time Complexity
+//      a) Worst Case: O(n²)
+//      b) Best Case: Ω(n²)
+//      c) Average Case: Θ(n²) 
+//
+// 3. INSERTION SORT 
